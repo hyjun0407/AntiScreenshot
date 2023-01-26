4f5a7a39cd8b8a10661125b288512857130cb13b
IAMSYNC <36983642+hyjun0407@users.noreply.github.com>
2023-01-26 16:09:05 +0900

    Add files via upload

---
+++ entry.cpp
@@ -0,0 +1,53 @@
+#include "library/globals.h"
+
+NTSTATUS drv_main(PDRIVER_OBJECT driver_obj, PUNICODE_STRING registery_path)
+{
+	///////////////////////////////
+	UNREFERENCED_PARAMETER(registery_path);
+	///////////////////////////////
+
+	///////////////////////////////
+	PDEVICE_OBJECT dev_obj;
+	NTSTATUS status = STATUS_SUCCESS;
+	//////////////////////////////
+
+	///////////////////////////////
+	UNICODE_STRING dev_name, sym_link;
+
+	dev_name = ansi_to_unicode("\\Device\\Uranus_K");
+	sym_link = ansi_to_unicode("\\DosDevices\\Uranus_K");
+	///////////////////////////////
+
+	///////////////////////////////
+	status = IoCreateDevice(driver_obj, 0, &dev_name, file_device_mirrore, 0x00000100, 0, &dev_obj);
+	if (!NT_SUCCESS(status))
+		return status;
+	///////////////////////////////
+
+	///////////////////////////////
+	status = IoCreateSymbolicLink(&sym_link, &dev_name);
+	if (!NT_SUCCESS(status))
+		return status;
+	///////////////////////////////
+
+	///////////////////////////////
+	SetFlag(dev_obj->Flags, DO_BUFFERED_IO);
+
+	for (int t = 0; t <= IRP_MJ_MAXIMUM_FUNCTION; t++)
+		driver_obj->MajorFunction[t] = unsupported_io;
+
+	driver_obj->MajorFunction[IRP_MJ_CREATE] = create_io;
+	driver_obj->MajorFunction[IRP_MJ_CLOSE] = close_io;
+	driver_obj->MajorFunction[IRP_MJ_DEVICE_CONTROL] = ctl_io;
+	driver_obj->DriverUnload = NULL;
+
+	ClearFlag(dev_obj->Flags, DO_DEVICE_INITIALIZING);
+	///////////////////////////////
+
+	return status;
+}
+
+NTSTATUS entry_point(PVOID a1, PVOID a2)
+{
+	return IoCreateDriver(&ansi_to_unicode("\\Driver\\Uranus_K"), &drv_main);
+}
